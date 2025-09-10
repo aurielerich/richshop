51356492b8a4554716bfbfd3c1e884e18022a4c7
aurielerich <aurielerich@gmail.com>
2025-09-10 11:02:45 +0700

    merubah tutorial 0 dan 1

--- main/models.py
+++ main/models.py
@@ -1,32 +1,72 @@
 import uuid
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-class News(models.Model):
+class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('transfer', 'Transfer'),
-        ('update', 'Update'),
-        ('exclusive', 'Exclusive'),
-        ('match', 'Match'),
-        ('rumor', 'Rumor'),
-        ('analysis', 'Analysis'),
+        ('JERSEY', 'Jersey'),
+        ('SEPATU', 'Sepatu Basket'),
+        ('BOLA', 'Bola Basket'),
+        ('APPAREL', 'Pakaian & Apparel'),
+        ('AKSESORIS', 'Aksesoris'),
+        ('KOLEKTIBEL', 'Kolektibel & Merchandise'),
     ]
     
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # title = models.CharField(max_length=255)
+    # content = models.TextField()
+    # category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
+    # thumbnail = models.URLField(blank=True, null=True)
+    # news_views = models.PositiveIntegerField(default=0)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # is_featured = models.BooleanField(default=False)
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    description = models.TextField()
     thumbnail = models.URLField(blank=True, null=True)
-    news_views = models.PositiveIntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='update')
     is_featured = models.BooleanField(default=False)
+    stock = models.IntegerField(default=0)
+    rating = models.FloatField(default=0.0,validators=[MinValueValidator(0.0), MaxValueValidator(5.0)])
+    rating_count = models.IntegerField(default=0)
+    total_rating_sum = models.IntegerField(default=0)
     
     def __str__(self):
         return self.title
     
     @property
-    def is_news_hot(self):
-        return self.news_views > 20
+    # def is_news_hot(self):
+    #     return self.news_views > 20
         
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
+    # def increment_views(self):
+    #     self.stock+= 1
+    #     self.save()
+    def __str__(self):
+        return self.name
+    
+    def is_in_stock(self):
+        return self.stock > 0
+        
+    def decrease_stock(self, quantity=1):
+        if self.stock >= quantity:
+            self.stock -= quantity
+            self.save()
+            return True
+        return False
+
+    def increase_stock(self, quantity=1):
+        if quantity > 0:
+            self.stock += quantity
+            self.save()
+
+    def set_stock(self, new_stock_level):
+        if new_stock_level >= 0:
+            self.stock = new_stock_level
+            self.save()
+
+    def update_rating(self, new_rating: int):
+        if 1 <= new_rating <= 5:
+            self.total_rating_sum += new_rating
+            self.rating_count += 1
+            self.rating = round(self.total_rating_sum / self.rating_count, 2)
+            self.save()
--- main/views.py
+++ main/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def show_main(request):
+    products = Product.objects.all()
     context = {
         'nama_project' : "Rich Shop",
         'npm' : '2406428806',
         'name': 'Auriel Erich Ibrahim Nst',
         'class': 'PBP F',
+        'products': products,
     }
-
     return render(request, "main.html", context)
